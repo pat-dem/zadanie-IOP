ed810439d3aaf5c2530c3a9faece88c18162e98d
Pat-dem <patdem@mat.umk.pl>
2026-01-20 17:22:24 +0100

    Dodano walidacje czy liczba jest dodatnia

--- projekt.cpp
+++ projekt.cpp
@@ -3,6 +3,12 @@
 int main() {
     int val;
     std::cin >> val;
+
+    if (val <= 0) {
+        std::cerr << "Blad: liczba nie jest dodatnia" << std::endl;
+        return 1;
+    }
+
     std::cout << val << std::endl;
     return 0;
 }
