2deff2f1c4c8408686fb4edffc35973180b00f56
Pat-dem <patdem@mat.umk.pl>
2026-01-20 17:16:38 +0100

    Dodano sprawdzanie liczby pierwszej

--- projekt.cpp
+++ projekt.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
+bool czy_pierwsza(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int val;
     std::cin >> val;
-    std::cout << val << std::endl;
+    std::cout << val;
+    if (czy_pierwsza(val)) {
+        std::cout << " jest liczba pierwsza" << std::endl;
+    } else {
+        std::cout << " nie jest liczba pierwsza" << std::endl;
+    }
     return 0;
 }
